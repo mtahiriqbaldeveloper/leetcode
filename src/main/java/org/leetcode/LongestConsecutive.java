087a68c6a3e2fe9aaeadd1d88574fb21b0e795c2
Tahir Iqbal <tahiriqbaldeveloper@gmail.com>
2023-10-16 14:01:39 +0200

    Binary Sum Arrays with Sum

---
+++ src/main/java/org/leetcode/LongestConsecutive.java
@@ -0,0 +1,36 @@
+package org.leetcode;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+/**
+ *<B>Leetcode Problem no<B/>: 128
+ *Problem Type: Array
+ *Difficulty Level: medium
+ */
+public class LongestConsecutive {
+    public static void main(String ...args){
+
+        int[] numbs = {100,4,200,1,3,2};
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int no:numbs){
+            set.add(no);
+        }
+        int longestCount = 1;
+        for (Integer num: set){
+
+            if(!set.contains(num - 1)){
+                int tempCount = 1;
+                while (set.contains(num + 1)){
+                    tempCount++;
+                    num++;
+                }
+
+                longestCount = Math.max(tempCount, longestCount);
+            }
+        }
+
+        System.out.println(longestCount);
+    }
+}
