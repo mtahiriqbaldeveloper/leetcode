e276fca067cada7d042512838c8734de9059f074
Tahir Iqbal <tahiriqbaldeveloper@gmail.com>
2023-11-01 00:09:40 +0100

    ThreeSumClosest updated better solution

--- src/main/java/org/leetcode/ThreeSumClosest.java
+++ src/main/java/org/leetcode/ThreeSumClosest.java
@@ -7,26 +7,41 @@ public class ThreeSumClosest {
 
     public static void main(String ... args){
 
-        int[] nums = {0,0,0,0};
+        int[] nums = {-1,2,1,-4};
         int target = 1;
-        int result = nums[0]+nums[1]+nums[nums.length-1];
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            int aPointer = i + 1;
-            int bPointer = nums.length - 1;
-            while (aPointer < bPointer){
-                int currentSum = nums[i]+ nums[aPointer]+ nums[bPointer];
-                if(currentSum > target){
-                    bPointer--;
-                }else {
-                    aPointer++;
+        int lowestDiff = Integer.MAX_VALUE;
+        final int n = nums.length;
+        int result = 0;
+
+        for (int i = 0; i < n -2 ; i++) {
+            int first = nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right){
+
+                int second = nums[left];
+                int third = nums[right];
+
+                int sum = first + second + third;
+
+                int diff = Math.abs(target - sum);
+                if( diff < lowestDiff){
+                    if(diff == 0){
+                        System.out.println(target);
+//                        return target;
+                    }
+                    lowestDiff = diff;
+                    result = sum;
                 }
-                if(Math.abs(currentSum-target)<Math.abs(result - target)){
-                    result = currentSum;
+                if(sum < target){
+                    left++;
+                }else {
+                    right--;
                 }
             }
         }
-
         System.out.println(result);
+//        return result;
     }
 }
